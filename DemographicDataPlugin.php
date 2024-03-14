d0b539facfee38c2d838680f0e855b0fbfd96d65
yves <yves@lepidus.com.br>
2024-03-14 17:30:15 -0400

    Initial project.
    Issue: documentacao-e-tarefas/scielo#643
    
    Signed-off-by: Yves <yves@lepidus.com.br>
    Signed-off-by: Iudi <iudi@lepidus.com.br>

---
+++ DemographicDataPlugin.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace APP\plugins\generic\demographicData;
+
+use PKP\plugins\GenericPlugin;
+use APP\core\Application;
+
+class DemographicDataPlugin extends GenericPlugin
+{
+    public function register($category, $path, $mainContextId = null): bool
+    {
+        $success = parent::register($category, $path);
+        return $success;
+    }
+
+    public function getDisplayName()
+    {
+        return __('plugins.generic.demographicData.displayName');
+    }
+
+    public function getDescription()
+    {
+        return __('plugins.generic.demographicData.description');
+    }
+
+    public function getCanEnable()
+    {
+        $request = Application::get()->getRequest();
+        return $request->getContext() !== null;
+    }
+
+    public function getCanDisable()
+    {
+        $request = Application::get()->getRequest();
+        return $request->getContext() !== null;
+    }
+}
