0bf4bc15262328d196a5010582eb5345d04a1224
yves <yves@lepidus.com.br>
2024-03-14 18:36:05 -0400

    Implements schema migration to add "demographic_questions" and "demographic_question_settings" tables.
    Issue: documentacao-e-tarefas/scielo#643
    
    Signed-off-by: Yves <yves@lepidus.com.br>
    Signed-off-by: Iudi <iudi@lepidus.com.br>

---
+++ DemographicQuestionsSchemaMigration.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace APP\plugins\generic\demographicData;
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class DemographicQuestionsSchemaMigration extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('demographic_questions', function (Blueprint $table) {
+            $table->bigInteger('demographic_question_id')->autoIncrement();
+            $table->bigInteger('context_id');
+
+            $table->foreign('context_id')
+                ->references('journal_id')
+                ->on('journals')
+                ->onDelete('cascade');
+            $table->index(['context_id'], 'demographic_questions_context_id');
+        });
+
+        Schema::create('demographic_question_settings', function (Blueprint $table) {
+            $table->bigIncrements('demographic_question_setting_id');
+            $table->bigInteger('demographic_question_id');
+            $table->string('locale', 14)->default('');
+            $table->string('setting_name', 255);
+            $table->longText('setting_value')->nullable();
+
+            $table->foreign('demographic_question_id')
+                ->references('demographic_question_id')
+                ->on('demographic_questions')
+                ->onDelete('cascade');
+
+            $table->index(['demographic_question_id'], 'demographic_question_settings_id');
+            $table->unique(['demographic_question_id', 'locale', 'setting_name'], 'demographic_question_settings_pkey');
+        });
+    }
+}
