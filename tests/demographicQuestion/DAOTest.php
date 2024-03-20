ca3a53a76a3ec3f4eb127e5e52419783a66aa19b
yves <yves@lepidus.com.br>
2024-03-20 18:01:14 -0400

    Refactors DAOTest.
    Issue: documentacao-e-tarefas/scielo#645
    
    Signed-off-by: Yves <yves@lepidus.com.br>
    Signed-off-by: Iudi <iudi@lepidus.com.br>

--- tests/demographicQuestion/DAOTest.php
+++ tests/demographicQuestion/DAOTest.php
@@ -5,9 +5,14 @@ namespace APP\plugins\generic\demographicData\tests\demographicQuestion;
 use APP\plugins\generic\demographicData\classes\demographicQuestion\DemographicQuestion;
 use APP\plugins\generic\demographicData\classes\demographicQuestion\DAO;
 use PKP\tests\DatabaseTestCase;
+use PKP\db\DAORegistry;
+use APP\journal\Journal;
 
 class DAOTest extends DatabaseTestCase
 {
+    private $context;
+    private $demographicQuestionDAO;
+
     protected function getAffectedTables(): array
     {
         return [
@@ -17,10 +22,29 @@ class DAOTest extends DatabaseTestCase
         ];
     }
 
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->demographicQuestionDAO = app(DAO::class);
+        $this->context = $this->createJournalMock();
+    }
+
+    private function createJournalMock()
+    {
+        $journal = $this->getMockBuilder(Journal::class)
+            ->getMock();
+        $journal->setName('journal-title', 'en');
+        $journal->setData('publisherInstitution', 'journal-publisher');
+        $journal->setPrimaryLocale('en');
+        $journal->setPath('journal-path');
+        $journal->setId(1);
+
+        return $journal;
+    }
+
     public function testCreateNewDataObject(): void
     {
-        $demographicQuestionDAO = app(DAO::class);
-        $demographicQuestion = $demographicQuestionDAO->newDataObject();
+        $demographicQuestion = $this->demographicQuestionDAO->newDataObject();
         self::assertInstanceOf(DemographicQuestion::class, $demographicQuestion);
     }
 }
