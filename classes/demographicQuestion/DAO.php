8a35bf837319e30be4b60eb04f827a7e8cce0122
yves <yves@lepidus.com.br>
2024-03-20 18:26:36 -0400

    Implements DAO add() and get() methods.
    Issue: documentacao-e-tarefas/scielo#645
    
    Signed-off-by: Yves <yves@lepidus.com.br>
    Signed-off-by: Iudi <iudi@lepidus.com.br>

--- classes/demographicQuestion/DAO.php
+++ classes/demographicQuestion/DAO.php
@@ -5,9 +5,12 @@ namespace APP\plugins\generic\demographicData\classes\demographicQuestion;
 use PKP\core\EntityDAO;
 use PKP\services\PKPSchemaService;
 use Illuminate\Support\LazyCollection;
+use PKP\core\traits\EntityWithParent;
 
 class DAO extends EntityDAO
 {
+    use EntityWithParent;
+
     public $schema = 'demographicQuestion';
     public $table = 'demographic_questions';
     public $settingsTable = 'demographic_question_settings';
@@ -17,8 +20,18 @@ class DAO extends EntityDAO
         'contextId' => 'context_id',
     ];
 
+    public function getParentColumn(): string
+    {
+        return 'context_id';
+    }
+
     public function newDataObject(): DemographicQuestion
     {
         return app(DemographicQuestion::class);
+    }
+
+    public function insert(DemographicQuestion $demographicQuestion): int
+    {
+        return parent::_insert($demographicQuestion);
     }
 }
--- tests/demographicQuestion/DAOTest.php
+++ tests/demographicQuestion/DAOTest.php
@@ -7,6 +7,7 @@ use APP\plugins\generic\demographicData\classes\demographicQuestion\DAO;
 use PKP\tests\DatabaseTestCase;
 use PKP\db\DAORegistry;
 use APP\journal\Journal;
+use PKP\plugins\Hook;
 
 class DAOTest extends DatabaseTestCase
 {
@@ -27,12 +28,18 @@ class DAOTest extends DatabaseTestCase
         parent::setUp();
         $this->demographicQuestionDAO = app(DAO::class);
         $this->context = $this->createJournalMock();
+        $this->addSchemaFile('demographicQuestion');
     }
 
     private function createJournalMock()
     {
         $journal = $this->getMockBuilder(Journal::class)
+            ->onlyMethods(['getId'])
             ->getMock();
+
+        $journal->expects($this->any())
+            ->method('getId')
+            ->will($this->returnValue(1));
         $journal->setName('journal-title', 'en');
         $journal->setData('publisherInstitution', 'journal-publisher');
         $journal->setPrimaryLocale('en');
@@ -46,5 +53,52 @@ class DAOTest extends DatabaseTestCase
     {
         $demographicQuestion = $this->demographicQuestionDAO->newDataObject();
         self::assertInstanceOf(DemographicQuestion::class, $demographicQuestion);
+    }
+
+    public function testCreateDemographicQuestion(): void
+    {
+        $locale = 'en';
+
+        $demographicQuestion = $this->demographicQuestionDAO->newDataObject();
+        $demographicQuestion->setContextId($this->context->getId());
+        $demographicQuestion->setQuestionText('Test title', $locale);
+        $demographicQuestion->setQuestionDescription('Test description', $locale);
+        $insertedDemographicQuestionId = $this->demographicQuestionDAO->insert($demographicQuestion);
+
+        $fetchedDemographicQuestion = $this->demographicQuestionDAO->get($insertedDemographicQuestionId, $this->context->getId());
+        self::assertEquals([
+            'id' => $insertedDemographicQuestionId,
+            'contextId' => $this->context->getId(),
+            'questionText' => ['en' => 'Test title'],
+            'questionDescription' => ['en' => 'Test description'],
+        ], $fetchedDemographicQuestion->_data);
+    }
+
+    private function addSchemaFile(string $schemaName): void
+    {
+        Hook::add(
+            'Schema::get::' . $schemaName,
+            function (string $hookName, array $args) use ($schemaName) {
+                $schema = &$args[0];
+
+                $schemaFile = sprintf(
+                    '%s/plugins/generic/demographicData/schemas/%s.json',
+                    BASE_SYS_DIR,
+                    $schemaName
+                );
+                if (file_exists($schemaFile)) {
+                    $schema = json_decode(file_get_contents($schemaFile));
+                    if (!$schema) {
+                        throw new \Exception(
+                            'Schema failed to decode. This usually means it is invalid JSON. Requested: '
+                            . $schemaFile
+                            . '. Last JSON error: '
+                            . json_last_error()
+                        );
+                    }
+                }
+                return true;
+            }
+        );
     }
 }
