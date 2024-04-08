b5f344314c1fab9d73f017bc6cbfccea1788b348
yves <yves@lepidus.com.br>
2024-04-08 15:41:39 -0400

    Adds getMany and fromRow methods.
    Issue: documentacao-e-tarefas/scielo#650
    
    Signed-off-by: Yves <yves@lepidus.com.br>
    Signed-off-by: Iudi <iudi@lepidus.com.br>

--- classes/demographicResponse/DAO.php
+++ classes/demographicResponse/DAO.php
@@ -44,4 +44,22 @@ class DAO extends EntityDAO
     {
         return parent::_update($demographicResponse);
     }
+
+    public function getMany(Collector $query): LazyCollection
+    {
+        $rows = $query
+            ->getQueryBuilder()
+            ->get();
+
+        return LazyCollection::make(function () use ($rows) {
+            foreach ($rows as $row) {
+                yield $row->demographic_response_id => $this->fromRow($row);
+            }
+        });
+    }
+
+    public function fromRow(object $row): DemographicQuestion
+    {
+        return parent::fromRow($row);
+    }
 }
