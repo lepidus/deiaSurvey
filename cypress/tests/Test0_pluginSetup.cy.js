5d0cc17b7b3654c0370842150a2cb7e97673ded4
Jhon <jhon@lepidus.com.br>
2024-05-29 14:43:49 -0400

    Adds integration test for plugin setup
    Issue: documentacao-e-tarefas/scielo#663
    
    Signed-off-by: Jhon <jhon@lepidus.com.br>

---
+++ cypress/tests/Test0_pluginSetup.cy.js
@@ -0,0 +1,13 @@
+describe('Plugin setup', function () {
+    it('Enables Demographic Data plugin"', function () {
+		cy.login('dbarnes', null, 'publicknowledge');
+
+		cy.contains('a', 'Website').click();
+
+		cy.waitJQuery();
+		cy.get('#plugins-button').click();
+
+		cy.get('input[id^=select-cell-demographicdataplugin]').check();
+		cy.get('input[id^=select-cell-demographicdataplugin]').should('be.checked');
+    });
+});
