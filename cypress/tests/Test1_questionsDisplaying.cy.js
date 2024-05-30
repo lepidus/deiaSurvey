62d6f2ce05d98da6f0b95db9f5833eb82462b89a
Jhon <jhon@lepidus.com.br>
2024-05-30 15:48:33 -0400

    Integration test - Demographic data consent
    Issue: documentacao-e-tarefas/scielo#663
    
    Signed-off-by: Jhon <jhon@lepidus.com.br>

--- cypress/tests/Test1_questionsDisplaying.cy.js
+++ cypress/tests/Test1_questionsDisplaying.cy.js
@@ -6,9 +6,8 @@ function assertDefaultQuestionsDisplay() {
     cy.contains('.description', 'How would you identify yourself in terms of ethnicity?');
 }
 
-
 describe('Questions displaying', function () {
-    it('Checks display of questions at users profile page', function () {
+    it('Display of questions at users profile page', function () {
         cy.login('dbarnes', null, 'publicknowledge');
         cy.get('.app__headerActions button').eq(1).click();
         cy.contains('a', 'Edit Profile').click();
@@ -20,4 +19,25 @@ describe('Questions displaying', function () {
         cy.contains('a', 'Edit Profile').click();
         assertDefaultQuestionsDisplay();
     });
+    it('User can choose not to answer questions', function () {
+        cy.login('dsokoloff', null, 'publicknowledge');
+        cy.get('.app__headerActions button').eq(1).click();
+        cy.contains('a', 'Edit Profile').click();
+        cy.contains('a', 'Demographic Data').click();
+
+        cy.contains('I agree to the processing of my Demographic Data');
+        cy.contains('I do not consent to the processing of my Demographic Data');
+
+        cy.get('input[name="demographicDataConsent"][value=0]').should('not.be.checked');
+        cy.get('input[name="demographicDataConsent"][value=1]').should('not.be.checked');
+        
+        cy.get('input[name="demographicDataConsent"][value=0]').click();
+        cy.get('#demographicDataForm .submitFormButton').click();
+        cy.wait(1000);
+        cy.reload();
+
+        cy.contains('a', 'Demographic Data').click();
+        cy.get('input[name="demographicDataConsent"][value=0]').should('be.checked');
+    });
+    //User answer questions
 });
