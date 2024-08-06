a41c860f1864c9822515ddde42c5a768131ec900
Jhon <jhon@lepidus.com.br>
2024-08-06 15:40:42 -0400

    Adds new questions check to integration test
    Issue: documentacao-e-tarefas/scielo#689
    
    Signed-off-by: Jhon <jhon@lepidus.com.br>

--- cypress/tests/Test1_questionsDisplaying.cy.js
+++ cypress/tests/Test1_questionsDisplaying.cy.js
@@ -1,9 +1,37 @@
 function assertDefaultQuestionsDisplay() {
     cy.contains('a', 'Demographic Data').click();
-    cy.contains('Gender');
+    cy.contains('label', 'Gender');
     cy.contains('.description', 'With which gender do you most identify?');
-    cy.contains('Ethnicity');
+
+    cy.contains('label', 'Ethnicity');
     cy.contains('.description', 'How would you identify yourself in terms of ethnicity?');
+
+    cy.contains('label', 'Academic background');
+    cy.contains('.description', 'Please tell us which academic institutions you have been involved with');
+
+    cy.contains('label', 'Languages');
+    cy.contains('.description', 'Which of these languages do you speak?');
+    cy.contains('English');
+    cy.contains('French');
+    cy.contains('Hindi');
+    cy.contains('Mandarin');
+    cy.contains('Portuguese');
+    cy.contains('Spanish');
+
+    cy.contains('label', 'Nacionality');
+    cy.contains('.description', 'Which continent are you from?');
+    cy.contains('Africa');
+    cy.contains('America');
+    cy.contains('Asia');
+    cy.contains('Europe');
+    cy.contains('Oceania');
+
+    cy.contains('label', 'Salary');
+    cy.contains('.description', 'What range is your current salary in?');
+    cy.contains('option', 'Less than a minimum wage');
+    cy.contains('option', 'One to three minimum wages');
+    cy.contains('option', 'Three to five minimum wages');
+    cy.contains('option', 'More than five minimum wages');
 }
 
 describe('Demographic Data - Questions displaying', function () {
