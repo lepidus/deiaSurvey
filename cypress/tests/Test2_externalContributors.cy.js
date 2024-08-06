905cce085e1d4d330d3fc0f16471fbbdf2373184
Jhon <jhon@lepidus.com.br>
2024-08-06 18:34:48 -0400

    Adds integration test - Display of question types in questionnaire page
    Issue: documentacao-e-tarefas/scielo#689
    
    Signed-off-by: Jhon <jhon@lepidus.com.br>

--- cypress/tests/Test2_externalContributors.cy.js
+++ cypress/tests/Test2_externalContributors.cy.js
@@ -1,5 +1,41 @@
 import '../support/commands.js';
 
+function assertDefaultQuestionsDisplay() {
+    cy.contains('a', 'Demographic Data').click();
+    cy.contains('label', 'Gender');
+    cy.contains('With which gender do you most identify?');
+
+    cy.contains('label', 'Ethnicity');
+    cy.contains('How would you identify yourself in terms of ethnicity?');
+
+    cy.contains('label', 'Academic background');
+    cy.contains('Please tell us which academic institutions you have been involved with');
+
+    cy.contains('label', 'Languages');
+    cy.contains('Which of these languages do you speak?');
+    cy.contains('English');
+    cy.contains('French');
+    cy.contains('Hindi');
+    cy.contains('Mandarin');
+    cy.contains('Portuguese');
+    cy.contains('Spanish');
+
+    cy.contains('label', 'Nacionality');
+    cy.contains('Which continent are you from?');
+    cy.contains('Africa');
+    cy.contains('America');
+    cy.contains('Asia');
+    cy.contains('Europe');
+    cy.contains('Oceania');
+
+    cy.contains('label', 'Salary');
+    cy.contains('What range is your current salary in?');
+    cy.contains('option', 'Less than a minimum wage');
+    cy.contains('option', 'One to three minimum wages');
+    cy.contains('option', 'Three to five minimum wages');
+    cy.contains('option', 'More than five minimum wages');
+}
+
 function beginSubmission(submissionData) {
     cy.get('input[name="locale"][value="en"]').click();
     cy.setTinyMceContent('startSubmission-title-control', submissionData.title);
@@ -133,10 +169,7 @@ describe('Demographic Data - External contributors data collecting', function() 
             });
         });
 
-        cy.contains('Gender');
-        cy.contains('With which gender do you most identify?');
-        cy.contains('Ethnicity');
-        cy.contains('How would you identify yourself in terms of ethnicity?');
+        assertDefaultQuestionsDisplay();
 
         cy.url().then(url => {
             cy.visit(url + 'breakToken');
