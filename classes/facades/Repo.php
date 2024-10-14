794a198d3ef9db0bb8651b9fd87ceeeb59353114
Jhon <jhon@lepidus.com.br>
2024-10-14 16:15:26 -0400

    Adds demographic response option repository in facade class
    Issue: documentacao-e-tarefas/scielo#706
    
    Signed-off-by: Jhon <jhon@lepidus.com.br>

--- classes/facades/Repo.php
+++ classes/facades/Repo.php
@@ -4,6 +4,7 @@ namespace APP\plugins\generic\demographicData\classes\facades;
 
 use APP\plugins\generic\demographicData\classes\demographicQuestion\Repository as DemographicQuestionRepository;
 use APP\plugins\generic\demographicData\classes\demographicResponse\Repository as DemographicResponseRepository;
+use APP\plugins\generic\demographicData\classes\demographicResponseOption\Repository as DemographicResponseOptionRepository;
 
 class Repo extends \APP\facades\Repo
 {
@@ -15,5 +16,10 @@ class Repo extends \APP\facades\Repo
     public static function demographicResponse(): DemographicResponseRepository
     {
         return app(DemographicResponseRepository::class);
+    }
+
+    public static function demographicResponseOption(): DemographicResponseOptionRepository
+    {
+        return app(DemographicResponseOptionRepository::class);
     }
 }
