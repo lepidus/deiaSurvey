792f2620cfff876cb6d3ce0547f079f47c048dc3
Jhon <jhon@lepidus.com.br>
2025-04-28 16:46:00 -0400

    Adds integration test - New settigs feature
    Issue: documentacao-e-tarefas/desenvolvimento_e_infra#990
    
    Signed-off-by: Jhon <jhon@lepidus.com.br>

--- cypress/tests/Test0_pluginSetup.cy.js
+++ cypress/tests/Test0_pluginSetup.cy.js
@@ -1,4 +1,7 @@
 describe('Demographic Data - Plugin setup', function () {
+	const pluginRowId = 'component-grid-settings-plugins-settingsplugingrid-category-generic-row-demographicdataplugin';
+	const orcidPluginRowId = 'dsfsdfad';
+	
 	it('Enables Demographic Data plugin. Editor does not give consent', function () {
 		cy.login('dbarnes', null, 'publicknowledge');
 
@@ -17,9 +20,31 @@ describe('Demographic Data - Plugin setup', function () {
         cy.get('#demographicDataForm .submitFormButton').click();
         cy.wait(1000);
 	});
-	it('Configures plugin', function() {
-		const pluginRowId = 'component-grid-settings-plugins-settingsplugingrid-category-generic-row-demographicdataplugin';
+	it("Plugin uses ORCID plugin's settings by default", function () {
+		cy.login('dbarnes', null, 'publicknowledge');
 
+		cy.contains('a', 'Website').click();
+
+		cy.waitJQuery();
+		cy.get('#plugins-button').click();
+
+		cy.get('tr#' + orcidPluginRowId + ' a.show_extras').click();
+		cy.get('a[id^=' + orcidPluginRowId + '-settings-button]').click();
+
+		cy.get('#orcidAPIPath').select('Public Sandbox');
+		cy.get('input[name="orcidClientId"]').clear().type(Cypress.env('orcidClientId'), {delay: 0});
+		cy.get('input[name="orcidClientSecret"]').clear().type(Cypress.env('orcidClientSecret'), {delay: 0});
+		cy.get('#orcidProfileSettingsForm button:contains("OK")').click();
+		cy.wait(1000);
+
+		cy.get('input[id^=select-cell-orcidprofileplugin]').check();
+		cy.get('input[id^=select-cell-orcidprofileplugin]').should('be.checked');
+
+		cy.get('tr#' + pluginRowId + ' a.show_extras').click();
+		cy.get('a[id^=' + pluginRowId + '-settings-button]').click();
+		cy.contains("The plugin is using the credentials entered in the ORCID Profile plugin settings. If you wish to use other credentials for this plugin, use the fields below");
+	});
+	it("Adds ORCID credentials to own plugin settings", function() {
 		cy.login('dbarnes', null, 'publicknowledge');
 		cy.contains('a', 'Website').click();
 
@@ -31,7 +56,11 @@ describe('Demographic Data - Plugin setup', function () {
 		cy.get('#orcidAPIPath').select('Public Sandbox');
 		cy.get('input[name="orcidClientId"]').clear().type(Cypress.env('orcidClientId'), {delay: 0});
 		cy.get('input[name="orcidClientSecret"]').clear().type(Cypress.env('orcidClientSecret'), {delay: 0});
-
 		cy.get('#demographicDataSettingsForm button:contains("OK")').click();
+		cy.wait(1000);
+
+		cy.get('tr#' + pluginRowId + ' a.show_extras').click();
+		cy.get('a[id^=' + pluginRowId + '-settings-button]').click();
+		cy.contains("This plugin is using the credentials below");
 	});
 });
