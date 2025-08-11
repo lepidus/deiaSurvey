929139c7a99d8cc4e897d7135f160db282190cb3
Jhon <jhon@lepidus.com.br>
2025-08-11 15:56:42 -0400

    Adds new migration for execution on plugin upgrading
    Issue: documentacao-e-tarefas/scielo#811
    
    Signed-off-by: Jhon <jhon@lepidus.com.br>

---
+++ classes/migrations/LocalizeQuestionsTextsMigration.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace APP\plugins\generic\deiaSurvey\classes\migrations;
+
+use Illuminate\Database\Migrations\Migration;
+
+class LocalizeQuestionsTextsMigration extends Migration
+{
+    public function up(): void
+    {
+        error_log('Testing the migration');
+    }
+}
