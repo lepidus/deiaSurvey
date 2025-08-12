e2c44293537fea42e6ea7e7db074db5f9da77a35
Jhon <jhon@lepidus.com.br>
2025-08-12 14:35:25 -0400

    Migration localizes questions' text and description
    Issue: documentacao-e-tarefas/scielo#811
    
    Signed-off-by: Jhon <jhon@lepidus.com.br>

--- classes/migrations/LocalizeQuestionsTextsMigration.php
+++ classes/migrations/LocalizeQuestionsTextsMigration.php
@@ -3,11 +3,45 @@
 namespace APP\plugins\generic\deiaSurvey\classes\migrations;
 
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
+use APP\plugins\generic\deiaSurvey\classes\facades\Repo;
 
 class LocalizeQuestionsTextsMigration extends Migration
 {
+    private const PREVIOUS_STANDARD_QUESTIONS = ['Gender', 'Race', 'Ethnicity'];
+
     public function up(): void
     {
-        error_log('Testing the migration');
+        $allQuestions = Repo::demographicQuestion()->getCollector()->getMany();
+
+        foreach ($allQuestions as $question) {
+            if ($this->isPreviousStandardQuestion($question)) {
+                $questionName = strtolower($question->getData('questionText')['en']);
+
+                $this->cleanQuestionTextualData($question);
+                Repo::demographicQuestion()->edit($question, [
+                    'isTranslated' => true,
+                    'isDefaultQuestion' => true,
+                    'questionText' => "plugins.generic.deiaSurvey.defaultQuestion.$questionName.title",
+                    'questionDescription' => "plugins.generic.deiaSurvey.defaultQuestion.$questionName.description"
+                ]);
+            }
+        }
+    }
+
+    private function isPreviousStandardQuestion($question): bool
+    {
+        return is_null($question->getData('isTranslated'))
+            && is_null($question->getData('isDefaultQuestion'))
+            && is_array($question->getData('questionText'))
+            && in_array($question->getData('questionText')['en'], self::PREVIOUS_STANDARD_QUESTIONS);
+    }
+
+    private function cleanQuestionTextualData($question)
+    {
+        DB::table('demographic_question_settings')
+            ->where('demographic_question_id', $question->getId())
+            ->whereIn('setting_name', ['questionText', 'questionDescription'])
+            ->delete();
     }
 }
