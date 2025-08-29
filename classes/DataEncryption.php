dde4dc4df85da2e7c7883a039b1b7818abc211f0
Jhon <jhon@lepidus.com.br>
2025-08-29 18:13:42 -0400

    Adds new methods to encrypt/decrypt data
    Issue: documentacao-e-tarefas/scielo#819
    
    Signed-off-by: Jhon <jhon@lepidus.com.br>

--- classes/DataEncryption.php
+++ classes/DataEncryption.php
@@ -2,12 +2,13 @@
 
 namespace APP\plugins\generic\deiaSurvey\classes;
 
-use Firebase\JWT\JWT;
 use PKP\config\Config;
 use Exception;
 
 class DataEncryption
 {
+    private const ENCRYPTION_CIPHER = 'AES-256-CBC';
+
     public function secretConfigExists(): bool
     {
         try {
@@ -25,5 +26,50 @@ class DataEncryption
             throw new Exception("DEIA Survey - A secret must be set in the config file ('api_key_secret') so that keys can be encrypted and decrypted");
         }
         return $secret;
+    }
+
+    public function encryptString(string $plainText): string
+    {
+        $secret = $this->getSecretFromConfig();
+        $initializationVector = openssl_random_pseudo_bytes(
+            openssl_cipher_iv_length(self::ENCRYPTION_CIPHER)
+        );
+        $encryptedString = openssl_encrypt(
+            $plainText,
+            self::ENCRYPTION_CIPHER,
+            $secret,
+            0,
+            $initializationVector
+        );
+
+        if ($encryptedString === false) {
+            throw new Exception("DEIA Survey - Failed to encrypt string");
+        }
+
+        return base64_encode($initializationVector . '::' . $encryptedString);
+    }
+
+    public function decryptString(string $encryptedText): string
+    {
+        $secret = $this->getSecretFromConfig();
+        $data = base64_decode($encryptedText);
+        if ($data === false) {
+            throw new Exception("DEIA Survey - Invalid base64 encoded data");
+        }
+
+        list($initializationVector, $encryptedString) = explode('::', $data, 2);
+        $decrypted = openssl_decrypt(
+            $encryptedString,
+            self::ENCRYPTION_CIPHER,
+            $secret,
+            0,
+            $initializationVector
+        );
+
+        if ($decrypted === false) {
+            throw new Exception("DEIA Survey - Failed to decrypt string");
+        }
+
+        return $decrypted;
     }
 }
