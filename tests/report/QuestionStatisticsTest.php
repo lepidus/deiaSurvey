bcd55e43c44b3ea349cfacb975736e9eeb0508c4
Jhon <jhon@lepidus.com.br>
2026-01-23 14:39:47 -0400

    Creates questions stats class
    Issue: documentacao-e-tarefas/scielo#878
    
    Signed-off-by: Jhon <jhon@lepidus.com.br>

---
+++ tests/report/QuestionStatisticsTest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace APP\plugins\generic\deiaSurvey\tests\report;
+
+use PKP\tests\PKPTestCase;
+use APP\plugins\generic\deiaSurvey\report\classes\QuestionStatistics;
+
+class QuestionStatisticsTest extends PKPTestCase
+{
+    private $questionStatistics;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->questionStatistics = new QuestionStatistics();
+    }
+
+    public function testResponseOptionsCountStartsEmpty(): void
+    {
+        $this->assertEquals([], $this->questionStatistics->getAllCounts());
+    }
+
+    public function testCanIncrementCountForResponseOptions(): void
+    {
+        $firstResponseOptionId = 15;
+        $secondResponseOptionId = 16;
+
+        $this->questionStatistics->incrementOptionCount($firstResponseOptionId);
+        $this->assertEquals(1, $this->questionStatistics->getOptionCount($firstResponseOptionId));
+
+        $this->questionStatistics->incrementOptionCount($firstResponseOptionId);
+        $this->assertEquals(2, $this->questionStatistics->getOptionCount($firstResponseOptionId));
+
+        $this->questionStatistics->incrementOptionCount($secondResponseOptionId);
+        $this->assertEquals(1, $this->questionStatistics->getOptionCount($secondResponseOptionId));
+
+        $this->assertEquals(
+            [$firstResponseOptionId => 2, $secondResponseOptionId => 1],
+            $this->questionStatistics->getAllCounts()
+        );
+    }
+
+    public function testGetNonexistentResponseOptionCount(): void
+    {
+        $nonExistentResponseOptionId = 123;
+        $this->assertNull($this->questionStatistics->getOptionCount($nonExistentResponseOptionId));
+    }
+}
