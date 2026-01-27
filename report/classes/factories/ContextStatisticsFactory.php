70c1e798ffe7e9fff8b55718c44b202d6bf7833c
Jhon <jhon@lepidus.com.br>
2026-01-27 14:13:18 -0400

    Adds creation of printing guide to context stats factory
    Issue: documentacao-e-tarefas/scielo#878
    
    Signed-off-by: Jhon <jhon@lepidus.com.br>

--- report/classes/factories/ContextStatisticsFactory.php
+++ report/classes/factories/ContextStatisticsFactory.php
@@ -4,6 +4,7 @@ namespace APP\plugins\generic\deiaSurvey\report\classes\factories;
 
 use APP\plugins\generic\deiaSurvey\report\classes\ContextStatistics;
 use APP\plugins\generic\deiaSurvey\report\classes\factories\QuestionStatisticsFactory;
+use APP\plugins\generic\deiaSurvey\classes\DefaultQuestionsCreator;
 use APP\plugins\generic\deiaSurvey\classes\facades\Repo;
 use APP\plugins\generic\deiaSurvey\classes\DemographicDataDAO;
 
@@ -37,5 +38,49 @@ class ContextStatisticsFactory
         $contextStats->setUsersNoConsentCount($contextConsentStats['noConsentCount']);
 
         return $contextStats;
+    }
+
+    public function createContextStatsPrintingGuide(): array
+    {
+        $defaultQuestionsData = DefaultQuestionsCreator::getDefaultQuestionsData($this->contextId);
+        $printingGuide = [];
+        $contextQuestions = Repo::demographicQuestion()
+            ->getCollector()
+            ->filterByContextIds([$this->contextId])
+            ->getMany();
+
+        foreach ($defaultQuestionsData as $questionData) {
+            foreach ($contextQuestions as $question) {
+                if ($question->getData('questionText') === $questionData['questionText']) {
+                    $responseOptionsGuide = $this->createQuestionStatsPrintingGuide(
+                        $question->getId(),
+                        $questionData['responseOptions']
+                    );
+
+                    $printingGuide[$question->getId()] = $responseOptionsGuide;
+                }
+            }
+        }
+
+        return $printingGuide;
+    }
+
+    private function createQuestionStatsPrintingGuide(int $questionId, array $responseOptionsData): array
+    {
+        $responseOptionsGuide = [];
+        $responseOptions = Repo::demographicResponseOption()
+            ->getCollector()
+            ->filterByQuestionIds([$questionId])
+            ->getMany();
+
+        foreach ($responseOptionsData as $responseOptionData) {
+            foreach ($responseOptions as $option) {
+                if ($option->getData('optionText') === $responseOptionData['optionText']) {
+                    $responseOptionsGuide[] = $option->getId();
+                }
+            }
+        }
+
+        return $responseOptionsGuide;
     }
 }
