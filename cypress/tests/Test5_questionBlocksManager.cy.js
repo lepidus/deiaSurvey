14f5daa571c2dee2243ccae6da05ea1cca88558a
Thiago Matos <thiago@lepidus.com.br>
2026-05-22 14:56:34 -0400

    test: split question block import export coverage
    documentacao-e-tarefas/desenvolvimento_e_infra#1187

--- cypress/tests/Test5_questionBlocksManager.cy.js
+++ cypress/tests/Test5_questionBlocksManager.cy.js
@@ -6,10 +6,8 @@ describe('DEIA Survey - Question blocks manager', function () {
 	const questionBlock = {
 		title: 'Funding DEIA questions',
 		editedTitle: 'Funding and access DEIA questions',
-		importedTitle: 'Imported funding and access DEIA questions',
 		description: 'Questions about access to funding opportunities.',
 		editedDescription: 'Questions about access to funding and participation opportunities.',
-		importedDescription: 'Imported questions about access to funding and participation opportunities.',
 		firstQuestion: {
 			text: 'Are you a scholarship recipient?',
 			description: 'Select all funding sources that apply.',
@@ -162,148 +160,6 @@ describe('DEIA Survey - Question blocks manager', function () {
 		cy.wait(500);
 	}
 
-	function localizedValues(localizedData) {
-		return Object.values(localizedData || {});
-	}
-
-	function setLocalizedData(localizedData, value) {
-		Object.keys(localizedData || {}).forEach((locale) => {
-			localizedData[locale] = value;
-		});
-	}
-
-	function parseExportedQuestionBlocks(responseBody) {
-		return typeof responseBody === 'string' ? JSON.parse(responseBody) : responseBody;
-	}
-
-	function exportQuestionBlock(title) {
-		return rowWithText(title).invoke('attr', 'id').then((rowId) => {
-			const blockId = rowId.match(/-row-(.+)$/)[1];
-
-			return cy.get('input[name="csrfToken"]').first().invoke('val').then((csrfToken) => (
-				cy.request({
-					method: 'POST',
-					url: '/index.php/publicknowledge/$$$call$$$/plugins/generic/deia-survey/classes/controllers/grid/deia-question-block/deia-question-block-grid/export-selected-question-blocks',
-					form: true,
-					body: {
-						csrfToken,
-						'selectedDeiaQuestionBlocks[]': blockId
-					}
-				}).then((response) => parseExportedQuestionBlocks(response.body))
-			));
-		});
-	}
-
-	function importQuestionBlocks(payload) {
-		const uploadFileUrl = '/index.php/publicknowledge/$$$call$$$/plugins/generic/deia-survey/classes/controllers/grid/deia-question-block/deia-question-block-grid/upload-question-blocks-file';
-		const importUrl = '/index.php/publicknowledge/$$$call$$$/plugins/generic/deia-survey/classes/controllers/grid/deia-question-block/deia-question-block-grid/upload-question-blocks';
-
-		cy.get('#deiaQuestionBlockGridContainer').contains('a', 'Import').click({force: true});
-		cy.get('form#deiaQuestionBlocksImportForm').should('be.visible');
-
-		cy.window().then((win) => {
-			const file = new win.File(
-				[JSON.stringify(payload, null, 2)],
-				'deia-question-blocks-import.json',
-				{type: 'application/json'}
-			);
-			const formData = new win.FormData();
-			formData.append('uploadedFile', file);
-
-			return win.fetch(uploadFileUrl, {
-				method: 'POST',
-				body: formData,
-				credentials: 'same-origin'
-			}).then((response) => response.json());
-		}).then((uploadResponse) => {
-			expect(uploadResponse.status).to.be.true;
-			expect(uploadResponse.temporaryFileId).not.to.be.undefined;
-
-			return cy.get('input[name="csrfToken"]').first().invoke('val').then((csrfToken) => (
-				cy.request({
-					method: 'POST',
-					url: importUrl,
-					form: true,
-					body: {
-						csrfToken,
-						temporaryFileId: uploadResponse.temporaryFileId
-					}
-				})
-			));
-		});
-
-		closeModal();
-		closeModal();
-		openPluginSettings();
-		cy.get('#deiaQuestionBlockGridContainer').contains(localizedValues(payload.blocks[0].title)[0]);
-	}
-
-	function createImportPayloadFromExport(exportedPayload, block) {
-		const importPayload = JSON.parse(JSON.stringify(exportedPayload));
-
-		setLocalizedData(importPayload.blocks[0].title, block.importedTitle);
-		setLocalizedData(importPayload.blocks[0].description, block.importedDescription);
-
-		return importPayload;
-	}
-
-	function assertExportedQuestionBlockPayload(payload, blockTitle, blockDescription, block) {
-		expect(payload.schemaVersion).to.equal('1.0');
-		expect(payload.plugin).to.equal('deiaSurvey');
-		expect(payload.blocks).to.have.length(1);
-
-		const exportedBlock = payload.blocks[0];
-		const questions = Object.values(exportedBlock.questions || {});
-		expect(localizedValues(exportedBlock.title)).to.include(blockTitle);
-		expect(localizedValues(exportedBlock.description)).to.include(blockDescription);
-		expect(questions).to.have.length(2);
-
-		const firstQuestion = questions.find((question) => (
-			localizedValues(question.questionText).includes(block.firstQuestion.text)
-		));
-		const secondQuestion = questions.find((question) => (
-			localizedValues(question.questionText).includes(block.secondQuestion.text)
-		));
-
-		expect(firstQuestion).not.to.be.undefined;
-		expect(localizedValues(firstQuestion.questionDescription)).to.include(block.firstQuestion.description);
-		const firstQuestionOptions = Object.values(firstQuestion.responseOptions || {});
-		expect(firstQuestionOptions).to.have.length(2);
-		expect(firstQuestionOptions.some((option) => (
-			localizedValues(option.optionText).includes(block.firstQuestion.options[0]) && !option.hasInputField
-		))).to.be.true;
-		expect(firstQuestionOptions.some((option) => (
-			localizedValues(option.optionText).includes(block.firstQuestion.options[1]) && option.hasInputField
-		))).to.be.true;
-
-		expect(secondQuestion).not.to.be.undefined;
-		expect(localizedValues(secondQuestion.questionDescription)).to.include(block.secondQuestion.description);
-		const secondQuestionOptions = Object.values(secondQuestion.responseOptions || {});
-		expect(secondQuestionOptions).to.have.length(0);
-	}
-
-	function exportImportAndAssertQuestionBlock(block) {
-		exportQuestionBlock(block.editedTitle).then((exportedPayload) => {
-			assertExportedQuestionBlockPayload(exportedPayload, block.editedTitle, block.editedDescription, block);
-
-			const importPayload = createImportPayloadFromExport(exportedPayload, block);
-			importQuestionBlocks(importPayload);
-
-			openQuestionsTabForBlock(block.importedTitle);
-			assertCreatedQuestionsAreDisplayed(block);
-			closeModal();
-
-			exportQuestionBlock(block.importedTitle).then((importedExportPayload) => {
-				assertExportedQuestionBlockPayload(
-					importedExportPayload,
-					block.importedTitle,
-					block.importedDescription,
-					block
-				);
-			});
-		});
-	}
-
 	function assertCreatedQuestionsAreDisplayed(block) {
 		cy.get('#deiaQuestionGridContainer').contains(block.firstQuestion.text);
 		cy.get('#deiaQuestionGridContainer').contains(block.secondQuestion.text);
@@ -349,8 +205,6 @@ describe('DEIA Survey - Question blocks manager', function () {
 		moveRowBefore(questionBlock.editedTitle, defaultQuestionBlockTitle);
 		saveGridOrder('deiaQuestionBlockGridContainer');
 		assertRowOrder(questionBlock.editedTitle, defaultQuestionBlockTitle);
-
-		exportImportAndAssertQuestionBlock(questionBlock);
 
 		setBlockStatus(questionBlock.editedTitle);
 
