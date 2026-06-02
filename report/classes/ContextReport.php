e3ae41b8d12634dc61f1aac46e03dbf083b41472
Jhon <jhon@lepidus.com.br>
2026-06-02 17:10:12 -0400

    Creates functions to add responses and options to context report
    Issue: documentacao-e-tarefas/scielo#913
    
    Signed-off-by: Jhon <jhon@lepidus.com.br>

--- report/classes/ContextReport.php
+++ report/classes/ContextReport.php
@@ -4,15 +4,20 @@ namespace APP\plugins\generic\deiaSurvey\report\classes;
 
 use APP\plugins\generic\deiaSurvey\classes\deiaQuestionBlock\DeiaQuestionBlock;
 use APP\plugins\generic\deiaSurvey\classes\deiaQuestion\DeiaQuestion;
+use APP\plugins\generic\deiaSurvey\classes\deiaQuestion\DeiaResponse;
+use APP\plugins\generic\deiaSurvey\classes\deiaResponseOption\DeiaResponseOption;
 
 class ContextReport
 {
     private array $questionBlocks;
     private array $questions;
+    private array $responses;
+    private array $responseOptions;
 
     public function __construct()
     {
         $this->questionBlocks = $this->questions = [];
+        $this->responses = $this->responseOptions = [];
     }
 
     public function addQuestionBlock(DeiaQuestionBlock $block)
@@ -27,6 +32,21 @@ class ContextReport
         $blockQuestions = $this->questions[$questionBlockId] ?? [];
         $blockQuestions[] = $question;
         $this->questions[$questionBlockId] = $blockQuestions;
+    }
+
+    public function addResponse(DeiaResponse $response)
+    {
+        $userId = $response->getUserId();
+        $questionId = $response->getDeiaQuestionId();
+
+        $userResponses = $this->responses[$userId] ?? [];
+        $userResponses[$questionId] = $response;
+        $this->responses[$userId] = $userResponses;
+    }
+
+    public function addResponseOption(DeiaResponseOption $responseOption)
+    {
+        $this->responseOptions[$responseOption->getId()] = $responseOption;
     }
 
     public function sequenceOrderFunction($left, $right)
