7e92856c34306992bcc2a427e37ed27211092d16
Jhon <jhon@lepidus.com.br>
2026-06-02 14:53:29 -0400

    Adds unit test - Question headers
    Issue: documentacao-e-tarefas/scielo#913
    
    Signed-off-by: Jhon <jhon@lepidus.com.br>

--- tests/report/ContextReportTest.php
+++ tests/report/ContextReportTest.php
@@ -4,18 +4,21 @@ namespace APP\plugins\generic\deiaSurvey\tests\report;
 
 use PKP\tests\PKPTestCase;
 use APP\plugins\generic\deiaSurvey\classes\deiaQuestionBlock\DeiaQuestionBlock;
+use APP\plugins\generic\deiaSurvey\classes\deiaQuestion\DeiaQuestion;
 use APP\plugins\generic\deiaSurvey\report\classes\ContextReport;
 
 class ContextReportTest extends PKPTestCase
 {
     private $contextReport;
     private $questionBlocks;
+    private $questions;
     private $locale = 'en';
 
     protected function setUp(): void
     {
         parent::setUp();
         $this->questionBlocks = $this->createTestQuestionBlocks();
+        $this->questions = $this->createTestQuestions();
         $this->contextReport = $this->createTestContextReport();
     }
 
@@ -38,6 +41,31 @@ class ContextReportTest extends PKPTestCase
         return $questionBlocks;
     }
 
+    private function createTestQuestions(): array
+    {
+        $questions = [];
+        $questionsPerBlock = 3;
+        foreach ($this->questionBlocks as $questionBlock) {
+            for ($sequence = 1; $sequence <= $questionsPerBlock; $sequence++) {
+                $questionId = (($questionBlock->getSequence() - 1) * $questionsPerBlock) + $sequence;
+                $question = new DeiaQuestion();
+                $question->setAllData([
+                    'id' => $questionId,
+                    'questionBlockId' => $questionBlock->getId(),
+                    'sequence' => $sequence,
+                    'questionType' => DeiaQuestion::TYPE_CHECKBOXES,
+                    'questionText' => [
+                        $this->locale => "Question number $questionId"
+                    ]
+                ]);
+
+                $questions[] = $question;
+            }
+        }
+
+        return $questions;
+    }
+
     private function createTestContextReport(): ContextReport
     {
         $contextReport = new ContextReport();
@@ -46,14 +74,39 @@ class ContextReportTest extends PKPTestCase
             $contextReport->addQuestionBlock($questionBlock);
         }
 
+        foreach ($this->questions as $question) {
+            $contextReport->addQuestion($question);
+        }
+
         return $contextReport;
+    }
+
+    public function testReportGeneratesQuestionsPrintingGuide()
+    {
+        $printingGuide = $this->contextReport->getQuestionPrintingGuide();
+        $expectedPrintingGuide = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+        $this->assertEquals($expectedPrintingGuide, $printingGuide);
     }
 
     public function testContextReportHeaders()
     {
         $headers = $this->contextReport->getHeaders();
 
-        $expectedBlockHeader = ['Question block number 1', 'Question block number 2', 'Question block number 3'];
-        $this->assertEquals($expectedBlockHeader, $headers[0]);
+        $expectedBlocksHeader = ['Question block number 1', 'Question block number 2', 'Question block number 3'];
+        $this->assertEquals($expectedBlocksHeader, $headers[0]);
+
+        $expectedQuestionsHeader = [
+            'Question number 1',
+            'Question number 2',
+            'Question number 3',
+            'Question number 4',
+            'Question number 5',
+            'Question number 6',
+            'Question number 7',
+            'Question number 8',
+            'Question number 9'
+        ];
+        $this->assertEquals($expectedQuestionsHeader, $headers[1]);
     }
 }
